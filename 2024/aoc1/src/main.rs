21818de2a91ba9cbde0b3e0b6f7afa4e3f95e7e1
triciadang <tricia.dang3@gmail.com>
2024-12-02 14:54:46 -0600

    aoc1 fixed formatting

--- 2024/aoc1/src/main.rs
+++ 2024/aoc1/src/main.rs
@@ -9,41 +9,37 @@ fn similarity_score(){
     let contents = fs::read_to_string("input.txt")
     .expect("Should have been able to read the file");
 
-let num_lines = contents.lines().count();
+    let num_lines = contents.lines().count();
 
+    let num_collection = contents.split_whitespace().collect::<Vec<&str>>();
+    let mut split_vectors_left: Vec<i32> = Vec::new();
+    let mut split_vectors_right: Vec<i32> = Vec::new();
 
-println!("{contents}");
+    for i in 0..num_collection.len() {
+        let mut str_to_num: i32 = num_collection[i].parse().unwrap();
+        if i%2 == 0{
+            split_vectors_left.push(str_to_num);
+        }
+        else{
+            split_vectors_right.push(str_to_num);
+        }
 
-let num_collection = contents.split_whitespace().collect::<Vec<&str>>();
-let mut split_vectors_left: Vec<i32> = Vec::new();
-let mut split_vectors_right: Vec<i32> = Vec::new();
-
-for i in 0..num_collection.len() {
-    let mut str_to_num: i32 = num_collection[i].parse().unwrap();
-    if i%2 == 0{
-        split_vectors_left.push(str_to_num);
     }
-    else{
-        split_vectors_right.push(str_to_num);
+
+    split_vectors_left.sort();
+    split_vectors_right.sort();
+
+    let mut total_diff = 0;
+
+    for j in 0..split_vectors_left.len() {
+        
+        let mut num_times_appear = split_vectors_right.iter().filter(|&n| * n == split_vectors_left[j]).count();
+
+        total_diff += split_vectors_left[j] * num_times_appear as i32;
+
     }
 
-}
-
-split_vectors_left.sort();
-split_vectors_right.sort();
-
-let mut total_diff = 0;
-
-for j in 0..split_vectors_left.len() {
-    
-    let mut num_times_appear = split_vectors_right.iter().filter(|&n| * n == split_vectors_left[j]).count();
-
-    total_diff += split_vectors_left[j] * num_times_appear as i32;
-
-}
-
-println!("Total Difference: {total_diff}");
-
+    println!("Total Difference: {total_diff}");
 
 }
 
